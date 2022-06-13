024e701c123efa912f27e0379f6b38936740d110
Himanshu Raj <himanshurajkush@gmail.com>
2022-06-13 19:31:26 +0530

    initial pbrp bringup

--- device.mk
+++ device.mk
@@ -64,8 +64,8 @@ TARGET_SCREEN_WIDTH := 1080
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/qcom-caf/bootctrl \
-    vendor/qcom/opensource/commonsys-intf/display
+    hardware/qcom/bootctrl \
+    vendor/qcom/opensource/display-commonsys-intf/
 
 # Update engine
 PRODUCT_PACKAGES += \
